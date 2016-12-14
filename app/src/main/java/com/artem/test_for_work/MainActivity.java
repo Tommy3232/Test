0cf222935f69b4a457934a2b831cfbecfc0ad453
Tommy3232 <Timon.sunming@gmail.com>
2016-12-14 22:10:10 +1000

    Создал MainActivity класс и layout

---
+++ app/src/main/java/com/artem/test_for_work/MainActivity.java
@@ -0,0 +1,17 @@
+package com.artem.test_for_work;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 14.12.2016.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
