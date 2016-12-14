61f75da9fd51edbbe1f7cdc08e5dfcc59c39e029
Tommy3232 <Timon.sunming@gmail.com>
2016-12-15 00:10:19 +1000

    Создал TollBar на андроиде 4 версии не запускается с телефона. Протестим на амуляторе и посмотрим что делать дальше. В коментах к видео уроку читал что на андроид 4 версии не будет работать, но в дальнейшем эта проблемма решается. Устанавливаю эмулятор.

--- app/src/main/java/com/artem/test_for_work/MainActivity.java
+++ app/src/main/java/com/artem/test_for_work/MainActivity.java
@@ -2,16 +2,32 @@ package com.artem.test_for_work;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-/**
- * Created by Admin on 14.12.2016.
- */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+
+    }
+    private void initToolbar(){
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
