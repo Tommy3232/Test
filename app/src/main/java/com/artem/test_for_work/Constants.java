41802b99f11223e8012c21aadb0d2349f31dac74
Tommy3232 <Timon.sunming@gmail.com>
2016-12-15 19:01:30 +1000

    Настроил переходы с меню на таб.

---
+++ app/src/main/java/com/artem/test_for_work/Constants.java
@@ -0,0 +1,9 @@
+package com.artem.test_for_work;
+
+
+
+public class Constants {
+    private static final int TAB_ONE = 0;
+    protected static final int TAB_TWO = 1;
+    private static final int TAB_THREE = 2;
+}
