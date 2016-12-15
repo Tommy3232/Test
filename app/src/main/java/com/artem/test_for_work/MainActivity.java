427f59b7b7bb71c4167375788113d5f655ab047f
Tommy3232 <Timon.sunming@gmail.com>
2016-12-15 14:50:57 +1000

    В навигации создал новости

--- app/src/main/java/com/artem/test_for_work/MainActivity.java
+++ app/src/main/java/com/artem/test_for_work/MainActivity.java
@@ -1,6 +1,7 @@
 package com.artem.test_for_work;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
@@ -8,17 +9,23 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private static final int LAYOUT = R.layout.activity_main;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
 
     }
+
+
+
     private void initToolbar(){
         toolbar=(Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
@@ -30,5 +37,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+    private void initNavigationView() {
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
